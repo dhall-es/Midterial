bd571667933f13351e65b343e9abe53aff5d168f
dhall-es <dylan.hall777@escapestudios.ac.uk>
2026-01-23 11:31:02 +0000

    Initial commit

---
+++ Source/Midterial/Private/MidterialBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MidterialBPLibrary.h"
+#include "Midterial.h"
+
+UMidterialBPLibrary::UMidterialBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UMidterialBPLibrary::MidterialSampleFunction(float Param)
+{
+	return -1;
+}
+
